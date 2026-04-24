bc6e0bd423dd3730255e4b165e23cb6387f3aa5d
Salua-Sa <slnulx5@gmail.com>
2026-04-24 11:17:08 +0000

    fix task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-if chr(i) != 'q' and chr(i) != 'e':
-	print("{}".format("".join([chr(i) for i in range(97, 123)])), end="")
+print("{}".format("".join([chr(i) for i in range(97, 123) if chr(i) != 'q' and chr(i) != 'e':])), end="")
