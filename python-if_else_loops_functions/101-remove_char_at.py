40e0c9bbea82c5ff9063899826bd78ad57f4b70b
Salua-Sa <slnulx5@gmail.com>
2026-04-26 14:33:19 +0000

    Add task 14

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    result = ""
+    if n >= 0 and n < len(str):
+        for i in range(len(str)):
+            if i != n:
+            result += str[i]
+        return result
+    else:
+        return str
