410d1d36f148366cdb50b94c2ad3a948b18ac509
Salua-Sa <slnulx5@gmail.com>
2026-04-27 17:56:44 +0000

    Add task 2

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif >= len(my_list):
+        return my_list
+    else:
+        for i in len(my_list):
+            if i == idx:
+                my_list[i] = element
+                return my_list
