57d427b5a108ae568a70040d5e3dfe5e30f0e3fb
Salua-Sa <slnulx5@gmail.com>
2026-04-28 19:49:48 +0000

    fix task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -5,7 +5,7 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in range(len(my_list)):
         if my_list[i] == search:
-            new_list[i] == replace
+            new_list[i] = replace
         else:
-            new_list[i] == my_list[i]
+            new_list[i] = my_list[i]
     return new_list
