cbcafb246dd7443299f05f685877e831f3e1ba23
Salua-Sa <slnulx5@gmail.com>
2026-04-28 18:55:01 +0000

    Add task 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        ne_row = []
+        for num in row:
+            new_row.append(num * num)
+    new_matrix.append(ne_row)
+    return new_matrix
