900b07fb22512c990a035ca3ab963f99f7e5090d
Salua-Sa <slnulx5@gmail.com>
2026-04-28 14:03:59 +0000

    Add task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    max = 0
+    if len(my_list) = 0:
+        return None
+    else:
+        for i in my_list:
+            if my_list[i] > max:
+                max = my_list[i]
+        return max
