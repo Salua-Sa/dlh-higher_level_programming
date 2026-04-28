2bccd1f2d296b641f857641451334656bf692e84
Salua-Sa <slnulx5@gmail.com>
2026-04-28 14:33:05 +0000

    fix task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 
 
 def max_integer(my_list=[]):
-    max = my_list[0]
     if len(my_list) == 0:
         return None
-    elif len(my_list) == 1:
+    max = my_list[0]
+    if len(my_list) == 1:
         return my_list[0]
     else:
         for i in my_list:
