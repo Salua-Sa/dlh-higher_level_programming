0d8eae57d3440263ee4dccd456de4359fc96aba4
Salua-Sa <slnulx5@gmail.com>
2026-04-29 12:55:52 +0000

    Add task 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_key = 0
+    max_value = 0
+
+    for key, value in a_dictionary.items():
+        if max_value < value:
+            best_key = key
+    return best_key
