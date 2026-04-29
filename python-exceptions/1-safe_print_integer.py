38b5149c47c687396d32944ea8d3e17a72bcba17
Salua-Sa <slnulx5@gmail.com>
2026-04-29 18:50:01 +0000

    Add gtask 1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    try:
+        if value is int:
+            print("{:d}".format(value))
+            return True
+        else:
+            print("{:d}".format(value)
+            return False
+    except IndexError:
+        break
