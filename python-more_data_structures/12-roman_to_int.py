eb3f85acb2e7d7effd49f94193b8382fa7c58281
Salua-Sa <slnulx5@gmail.com>
2026-04-29 13:28:22 +0000

    Add task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+     int_num = 0
+    if roman_string not string or roman_string == None:
+        return int_num
+    else:
+        for i in roman_string:
+            if i == I:
+                int_num += 1
+            elif i == V:
+                int_num += 5
+            elif i == X:
+                int_num += 10
+            elif i == L:
+                int_num += 50
+            elif i == C:
+                int_num += 100
+            elif i == D:
+                int_num += 500
+            elif i == D:
+                int_num += 1000
+    return int_num
