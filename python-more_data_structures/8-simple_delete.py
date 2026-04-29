d58a634a365e3a0d62d8024d64ac66b55a1778b1
Salua-Sa <slnulx5@gmail.com>
2026-04-29 11:42:16 +0000

    Fix task 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,6 @@
 
 
 def simple_delete(a_dictionary, key=""):
-    for i in a_dictionary:
-        if i == key:
-            del a_dictionary[i]
+    if key in a_dictionary:
+        del a_dictionary[i]
     return a_dictionary
