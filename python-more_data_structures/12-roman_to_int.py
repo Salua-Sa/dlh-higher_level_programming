ea35a7770661132c30af6a97cc4b6f87a8995800
Salua-Sa <slnulx5@gmail.com>
2026-04-29 15:02:37 +0000

    fix task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,7 @@
 def roman_to_int(roman_string):
     values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total_value = 0
+    current_value = 0
     if roman_string == None:
         return int_num
     else:
