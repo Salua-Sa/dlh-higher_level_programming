b4099ec6984446f7255bfb19da3e726b879ba0ee
Salua-Sa <slnulx5@gmail.com>
2026-04-29 12:21:47 +0000

    Fix task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -4,5 +4,5 @@
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
     for key in a_dictionary:
-        new_dictioonary[key] = value * 2
+        new_dictionary[key] = value * 2
     returm new_dictionary
