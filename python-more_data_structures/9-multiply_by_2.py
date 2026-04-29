6f06547fa854fa70bbd7f7fdbf2e666c02a93259
Salua-Sa <slnulx5@gmail.com>
2026-04-29 12:25:55 +0000

    fix task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -4,5 +4,5 @@
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
     for key in a_dictionary:
-        new_dictionary[key] = value * 2
+        new_dictionary[key] = key * 2
     return new_dictionary
