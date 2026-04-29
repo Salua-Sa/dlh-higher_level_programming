2671cbd3e26a7a02e603e3c4725fc9b821d8cbab
Salua-Sa <slnulx5@gmail.com>
2026-04-29 19:13:57 +0000

    fix task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,9 +7,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             counter += 1
-        except IndexError:
-            continue
         except (ValueError, TypeError):
+            continue
+        except IndexError:
             break
     print()
     return counter
