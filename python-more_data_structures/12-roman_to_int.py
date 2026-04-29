84de488bc300ab27fe6d66a10e10b887b26893af
Salua-Sa <slnulx5@gmail.com>
2026-04-29 13:41:37 +0000

    fix task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -23,5 +23,17 @@ def roman_to_int(roman_string):
                     int_num += 1000
             else:
                 if i == "I":
-                    int_nume -= 1
+                    int_num -= 1
+                elif i == "V":
+                    int_num -= 5
+                elif i == "X":
+                    int_num -= 10
+                elif i == "L":
+                    int_num -= 50
+                elif i == "C":
+                    int_num -= 100
+                elif i == "D":
+                    int_num -= 500
+                elif i == "D":
+                    int_num -= 1000
     return int_num
