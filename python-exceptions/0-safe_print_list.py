a242e319042adfc4add57f23e0fb2d9f2ae6d8e5
Salua-Sa <slnulx5@gmail.com>
2026-04-29 18:36:26 +0000

    fix task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
         print(my_list[i], end="")
         counter += 1
     except IndexError:
-        print("the number of elements is bigger than the elements in thes list")
         break
+    print()
     return counter
