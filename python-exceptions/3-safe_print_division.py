3fe242648e0950b8bb3b8b68c2ff780fa020e3b8
Salua-Sa <slnulx5@gmail.com>
2026-04-30 07:56:49 +0000

    Fisx task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except ZeroDivisionError:
         return None
     finally:
         print("Inside result: {}".format(result)
