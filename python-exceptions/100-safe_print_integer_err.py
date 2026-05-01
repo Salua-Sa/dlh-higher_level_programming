19f338d56ba446ca30a973fa855839b03efd4b20
Salua-Sa <slnulx5@gmail.com>
2026-05-01 07:17:46 +0000

    fix task 7

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError):
+    except (TypeError, ValueError) as error:
         print("Exception: {}".format(error), file=__import__("sys").stderr)
         return False
