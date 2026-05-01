1f741375dfaee335102c2288219c4e18c8ac5813
Salua-Sa <slnulx5@gmail.com>
2026-05-01 06:36:42 +0000

    fix task 15

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 
 
 def complex_delete(a_dictionary, value):
-    for key in a_dictionary:
+    for key in list(a_dictionary.keys()):
         if a_dictionary[key] == value:
             del a_dictionary[key]
     return a_dictionary
