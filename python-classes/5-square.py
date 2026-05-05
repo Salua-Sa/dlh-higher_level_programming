54c2e4d3c8260f10efdf0968ad18e8dee97aaf88
Salua-Sa <slnulx5@gmail.com>
2026-05-05 22:13:33 +0200

    fix task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -23,6 +23,7 @@ class Square:
                 raise ValueError("size must be >= 0")
             self.__size = value
 
+
     def area(self):
         """This method returns the current square area."""
         return self.__size * self.__size
