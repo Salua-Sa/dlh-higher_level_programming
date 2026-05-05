c8b4e9160b62ca1568aba508aa924cee3d6c0cfa
Salua-Sa <slnulx5@gmail.com>
2026-05-05 15:04:00 +0200

    fix task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,7 +9,7 @@ class Square:
         """Initializes a Square with a given size and specific conditions."""
         if type(size) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
