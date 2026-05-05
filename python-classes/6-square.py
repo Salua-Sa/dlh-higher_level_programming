122bb4e89c8843446ee342443283d4c020076a5a
Salua-Sa <slnulx5@gmail.com>
2026-05-05 20:14:35 +0200

    Fix task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -49,7 +49,7 @@ class Square:
              for i in range(self.__size):
                  for j in range(self.__position[0]):
                      print("_", end="")
-                 for k in range(self.__size):
-                     print("#", end="")
+                     for k in range(self.__size):
+                         print("#", end="")
                  print("")
              
