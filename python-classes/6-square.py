b2503c2cdbf15f49be9c5f44c3bdf27c448de308
Salua-Sa <slnulx5@gmail.com>
2026-05-05 18:30:08 +0200

    Fix task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This module contains a Square class."""
+
+
+class Square:
+    """This class defines a square with size."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes a Square with a given size and position."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+         """Retrieves the size of the square."""
+         return self.__size
+
+    @size.setter
+    def size(self, value):
+            """Sets the size of the square."""
+            if type(value) != int:
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+    @property
+    def position(self):
+         """Retrieves the position of the square."""
+         return self.__position
+    
+    @position.setter
+    def position(self, value):
+        """Sets the size of the square."""
+        if type(value) != tuple or len(value) != 2 or type(value[0]) < 0 or type(value[1]) < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+
+    def area(self):
+        """This method returns the current square area."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """This method prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+             for i in range(self.__size):   
+                 print("#" * self.size)
+             print("")
