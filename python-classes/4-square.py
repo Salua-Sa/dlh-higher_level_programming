de0e5152dfdba2e262013df4ab162e370fa8134a
Salua-Sa <slnulx5@gmail.com>
2026-05-05 16:16:57 +0200

    fix task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,23 +6,22 @@ class Square:
     """This class defines a square with size."""
 
     def __init__(self, size=0):
-        """Initializes a Square with a given size and specific conditions."""
+        """Initializes a Square with a given size."""
+        self.size = size
 
-        @property
-        def size(self):
-            """Retrieves the size of the square."""
-            return self.__size
+    @property
+    def size(self):
+         """Retrieves the size of the square."""
+         return self.__size
 
-        @size.setter
-        def size(self, value):
+    @size.setter
+    def size(self, value):
             """Sets the size of the square."""
+            if type(value) != int:
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
             self.__size = value
-
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
 
     def area(self):
         """This method returns the current square area."""
