01ebd80b367f01ec03c842bda4db5967aa4cf85e
Salua-Sa <slnulx5@gmail.com>
2026-05-06 16:34:32 +0200

    Fix task 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -65,4 +65,4 @@ class Rectangle:
 
     def __repr__(self):
         """Returns a string representation of the rectangle """
-        return Rectangle("{}, {}".format(self.__width, self.__height))
+        return "Rectangle({}, {})".format(self.__width, self.__height)
