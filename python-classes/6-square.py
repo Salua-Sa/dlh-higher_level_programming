dfa290c33ba3da6e5b165e7805507c406368550a
Salua-Sa <slnulx5@gmail.com>
2026-05-06 10:30:39 +0200

    fix task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -34,7 +34,8 @@ class Square:
     def position(self, value):
         """Sets the position of the square."""
         if (type(value) is not tuple or len(value) is not 2 or value[0] < 0 or
-                type(value[0]) is not int or value[1] < 0 or type(value[1]) is not int):
+                type(value[0]) is not int or value[1] < 0 or
+                type(value[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -51,7 +52,7 @@ class Square:
                 print("")
             for i in range(self.__size):
                 for j in range(self.__position[0]):
-                    print("_", end="")
+                    print("", end="")
                 for k in range(self.__size):
                     print("#", end="")
                 print("")
