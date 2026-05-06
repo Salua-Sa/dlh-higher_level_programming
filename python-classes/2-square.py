58210f1787223fd347d4c6f11e7d1c2f2f2ebe02
Salua-Sa <slnulx5@gmail.com>
2026-05-06 09:40:33 +0200

    fix task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,15 +6,9 @@ class Square:
     """This class Square defines a square with size."""
 
     def __init__(self, size=0):
-        """Initializes a Square with a given size and specific conditions:
-        1) size must be an integer, otherwise raise a
-        TypeError exception with the message size must be an integer.
-        2) if size is less than 0, raise a ValueError
-        exception with the message size must be >= 0
+        """Initializes a Square with a given size and specific conditions"""
 
-        """
-
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
