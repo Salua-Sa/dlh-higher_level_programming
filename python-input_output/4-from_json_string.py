424c15cebe43550f3fc2d8843e8b86225303db9a
Salua-Sa <slnulx5@gmail.com>
2026-05-07 10:45:56 +0200

    Add task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Rturns an object (Python data structure) represented by a JSON string
+"""
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string
+    """
+    import json
+    return json.loads(my_str)
