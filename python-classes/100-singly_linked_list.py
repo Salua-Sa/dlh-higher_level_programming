165ebdf433c8e3c913ce429dbf532355c6ef47c8
Salua-Sa <slnulx5@gmail.com>
2026-05-10 14:55:43 +0200

    Fix task 7

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -59,7 +59,8 @@ class SinglyLinkedList:
             return
 
         current = self.__head
-        while current.next_node is not None and value >= current.next_node.data:
+        while (current.next_node is not None
+               and value >= current.next_node.data):
             current = current.next_node
 
         new_node.next_node = current.next_node
