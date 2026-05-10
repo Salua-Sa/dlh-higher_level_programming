54df2c0c095f7136373b2ea55174bf91998bb8fa
Salua-Sa <slnulx5@gmail.com>
2026-05-10 12:25:35 +0000

    Fix task 7

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ class Node that defines a node of a singly linked list"""
+
+
 class Node:
     def __init__(self, data, next_node=None):
-        """Initializes a Node with a given data and next_node."""
+        """Initializes a Node with a given data and next_node"""
         self.data = data
         self.next_node = next_node
 
@@ -11,37 +13,35 @@ class Node:
         """Retrieves the data of the node"""
         return selt.__data
 
-
     @data.setter
     def data(self, value):
-        """Sets the data of the node."""
-        if type(value) is not int:
+        """Sets the data of the node"""
+        if not isinstance(value, int):
             raise TypeError("data must be an integer")
         else:
             self.__data = value
-
 
     @property
     def next_node(self):
         """Retrieves the next_node of the node"""
         return selt.__next_node
 
-
     @next_node.setter
     def next_node(self, value):
-        """Sets the next_node of the node."""
-        if not isinstance(value, Node) and value is not  None:
+        """Sets the next_node of the node"""
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """Class SinglyLinkedList that defines a singly linked list """
+    """Class SinglyLinkedList that defines a singly linked list"""
     def __init__(self):
         """Initializes a head"""
         self.__head = None
 
     def __str__(self):
+        """Return the result"""
         current = self.__head
         result = ""
         while current is not None:
@@ -50,6 +50,7 @@ class SinglyLinkedList:
         return result.rstrip()
 
     def sorted_insert(self, value):
+        """Insert a new Node into the correct sorted"""
         new_node = Node(value)
         if self.__head is None or value < self.__head.data:
             new_node.next_node = self.__head
