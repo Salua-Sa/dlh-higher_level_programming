7b05bc1ef7b78ed2fc166743f1786f7a00b6cf91
Salua-Sa <slnulx5@gmail.com>
2026-05-24 19:02:07 +0200

    Fix task 14

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -29,6 +29,7 @@ def main():
                 if status_codes[code] > 0:
                     print("{}: {}".format(code, status_codes[code]))
                 print("File size: {}".format(total_size))
+    print("File size: {}".format(total_size))
     for code in sorted(status_codes):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
